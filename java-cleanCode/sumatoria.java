0ec09a943a8b1239d46d1b2e2e8ea8bf89a0717b
Luca-Javier <luca.jav45@gmail.com>
2022-09-08 23:22:03 -0300

    clean code

---
+++ java-cleanCode/sumatoria.java
@@ -0,0 +1,20 @@
+// import junit.framework.TestCase;
+
+public class sumatoria {
+  int numero1;
+  int numero2;
+
+  /*
+   * public static void main(String[] args) {
+   * }
+   */
+
+  public sumatoria(int numero1, int numero2) {
+    this.numero1 = numero1;
+    this.numero2 = numero2;
+  }
+
+  public int sumar() {
+    return this.numero1 + this.numero1;
+  }
+}
