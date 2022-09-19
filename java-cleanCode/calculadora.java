4601e78ccdb25f31d6761f7e95693f354882afc2
kk <luca.jav45mail.com>
2022-09-19 19:25:57 -0300

    fdf

--- java-cleanCode/calculadora.java
+++ java-cleanCode/calculadora.java
@@ -1,6 +1,6 @@
 // import junit.framework.TestCase;
 
-public class sumatoria {
+public class calculadora {
   int numero1;
   int numero2;
 
@@ -9,12 +9,18 @@ public class sumatoria {
    * }
    */
 
-  public sumatoria(int numero1, int numero2) {
+  public calculadora(int numero1, int numero2) {
     this.numero1 = numero1;
     this.numero2 = numero2;
   }
 
   public int sumar() {
     return this.numero1 + this.numero1;
+  }
+  public int restar() {
+    return this.numero1 - this.numero1;
+  }
+  public int dividir() {
+    return this.numero1 / this.numero1;
   }
 }
--- java-cleanCode/holaMundo.java
+++ java-cleanCode/holaMundo.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class holaMundo {
   public static void main(String[] args) {
     System.out.println("Hola mundo");
+    Scanner inputTeclado = new Scanner(System.in);
+    System.out.println("Numero 1: ");
+    int numero1 = Integer.parseInt(inputTeclado.nextLine());
+    System.out.println("Numero 2: ");
+    int numero2 = Integer.parseInt(inputTeclado.nextLine());
+
+    calculadora calculadora = new calculadora(numero1, numero2);
+
+    int resultado = calculadora.sumar();
+
+    System.out.println("El resultado es"+Integer.toString(resultado));
+
+    inputTeclado.close();
   }
 }
