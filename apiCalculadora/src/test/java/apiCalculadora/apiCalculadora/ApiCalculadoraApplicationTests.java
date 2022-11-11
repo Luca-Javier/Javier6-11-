b3d5901e69f28dba25502870693a488619ceecf8
Luca-Javier <luca.jav45@gmail.com>
2022-11-10 23:27:12 -0300

    test casi terminados

--- apiCalculadora/src/test/java/apiCalculadora/apiCalculadora/ApiCalculadoraApplicationTests.java
+++ apiCalculadora/src/test/java/apiCalculadora/apiCalculadora/ApiCalculadoraApplicationTests.java
@@ -13,22 +13,48 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class ApiCalculadoraApplicationTests {
 
-	ApiCalculadoraApplication calc = new ApiCalculadoraApplication();
+	ApiCalculadoraApplication calculator = new ApiCalculadoraApplication();
+
 	@Test
-	void contextLoads() {
+	public void sumaTest() {
+		assertEquals(calculator.sumar(1, 2), 3);
+		assertNotEquals(calculator.sumar(4,1),3);
 	}
 
 	@Test
-	void sumar() {
-
-		assertEquals(calc.sumar(1,2),3);
-		assertNotEquals(calc.sumar(1,2),4);
+	public void restarTest() {
+		assertEquals(calculator.restar(5, 2), 3);
+		assertNotEquals(calculator.restar(3,1),3);
 	}
-	@Test //(expected = ArithmeticException.class)
-	public void shouldThrowException() {
+
+	@Test
+	public void multiplicarTest() {
+		assertEquals(calculator.multiplicar(5, 2), 10);
+		assertNotEquals(calculator.multiplicar(4,1),3);
+	}
+
+	@Test
+	public void dividirTest() {
+		assertEquals(calculator.dividir(6, 2), 3);
+		assertNotEquals(calculator.dividir(6, 2), 4);
+		assertNotEquals(calculator.dividir(3, 2), 4);
+	}
+	@Test
+	public void dividirException() {
 		Assertions.assertThrows(ArithmeticException.class,()->{
-				calc.dividir(1,0);
+				calculator.dividir(1,0);
 		});
 
 	}
+	/*
+	@Test
+	public void raizCuadraticaTest(){
+		assertEquals(calculator.cuadratica(1,2,-8),);
+		assertNotEquals(calculator.cuadratica(1,2,-8),-45,25); //Los 2 resultados tienen que ser diferentes
+		assertNotEquals(calculator.cuadratica(0,2,-8),-45,25);
+		assertNotEquals(calculator.cuadratica(1,0,-8),-45,25);
+
+	}
+	*/
+	 
 }
